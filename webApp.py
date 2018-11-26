74b9d276ca94e812d31c220445a81d2ad7bbc134
Rutherford1895 <rutherford1895@gmail.com>
2018-11-26 13:50:43 +0100

    Developing WebUI

--- webApp.py
+++ webApp.py
@@ -2,7 +2,14 @@ from flexx import flx
 
 class Example(flx.Widget):
     def init(self):
-        flx.Label(text='To be developed')
+        flx.Label(text='Load and display a Stella SD Model')
+        self.variablesInModel = ["Variable"]
+        with flx.HBox():
+            flx.Button(text='Select model')
+            flx.Button(text='Run')
+            flx.ComboBox(options=self.variablesInModel, selected_index=0, style='width: 100%')
+            flx.Button(text='Show figure')
+            flx.Button(text='Reset canvas')
 
 app = flx.App(Example)
 
