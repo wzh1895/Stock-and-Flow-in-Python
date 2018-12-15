0469a969244f7a4ce4a2af7c40e5a766b170ab78
Rutherford1895 <rutherford1895@gmail.com>
2018-12-15 03:27:54 +0100

    Add Canvas

--- webApp.py
+++ webApp.py
@@ -1,17 +1,33 @@
-from flexx import flx
+from flexx import flx, event
 
-class Example(flx.Widget):
+class DisplayArea(flx.CanvasWidget):
+
+    # CSS's name should match the pattern "flx-$Classname"
+
+    CSS = """
+        .flx-DisplayArea {background: #fff; border: 5px solid #888;}
+        """
+
     def init(self):
-        flx.Label(text='Load and display a Stella SD Model')
-        self.variablesInModel = ["Variable"]
-        with flx.HBox():
-            flx.Button(text='Select model')
-            flx.Button(text='Run')
-            flx.ComboBox(options=self.variablesInModel, selected_index=0, style='width: 100%')
-            flx.Button(text='Show figure')
-            flx.Button(text='Reset canvas')
+        super().init()
+        self.ctx = self.node.getContext('2d')
 
-app = flx.App(Example)
+
+class SFDCanvas(flx.Widget):
+    def init(self):
+        self.variablesInModel = ["Variable"]
+        with flx.VBox(flex=1):
+            with flx.HBox():
+                flx.Label(text='Load and display a Stella SD Model')
+            with flx.HBox():
+                flx.Button(text='Select model')
+                flx.Button(text='Run')
+                flx.ComboBox(options=self.variablesInModel, selected_index=0, style='width: 100%')
+                flx.Button(text='Show figure')
+                flx.Button(text='Reset canvas')
+            self.display = DisplayArea(flex=1)
+
+app = flx.App(SFDCanvas)
 
 #app.export('example.html', link=0)  # Export to single file
 #app.launch('browser')  # show it now in a browser
