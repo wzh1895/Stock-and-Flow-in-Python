9966e6e4272c88995f6584ed91f3f88b5fd3b451
Rutherford1895 <rutherford1895@gmail.com>
2019-04-05 04:11:48 +0200

    Starting expansion.py

--- expansion.py
+++ expansion.py
@@ -3,12 +3,17 @@ main utility for model expansion
 """
 
 from tkinter import *
-from controller import ControllerBar
+from suggestion import SuggestionPanel
+
+
+class ExpansionPanel(SuggestionPanel):
+    def __init__(self, master, wid, hei):
+        super().__init__(master, wid, hei)
 
 
 def main():
     root = Tk()
-    expansion_test1 = ControllerBar(root)
+    expansion_test1 = ExpansionPanel(root, 485, 160)
     root.wm_title("Expansion Test")
     root.geometry("%dx%d+100+100" % (expansion_test1.wid, expansion_test1.hei))
     root.configure(background='#fff')
