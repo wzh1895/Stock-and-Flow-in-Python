3d2ce43022c107759c601e3f67fd4840214c33ad
rutherford1895 <rutherford1895@gmail.com>
2019-05-20 00:48:30 +0200

    Before merging CandidateStructureWindow to main window

--- expansion_test_1.py
+++ expansion_test_1.py
@@ -222,7 +222,7 @@ class StructureUtilities(object):
     @staticmethod
     def expand_structure(base_structure, target_structure):
         new_base = copy.deepcopy(base_structure)
-        print("    Base_structure: ", new_base.model_structure.sfd.nodes(data=True))
+        print("    Base_structure: ", new_base.model_structure.sfd.nodes(data='function', default='Not available'))
 
         # Base
 
@@ -231,7 +231,7 @@ class StructureUtilities(object):
         # pick an element from base_structure to start with. Now: randomly. Future: guided by activity.
         start_with_element_base = random.choice(base_structure_elements)
         print("    {} in base_structure is chosen to start with".format(start_with_element_base))
-        print("    Details: ", new_base.model_structure.sfd.nodes[start_with_element_base])
+        print("    Details: ", new_base.model_structure.sfd.nodes[start_with_element_base]['function'])
 
         # get all in_edges into this element in base_structure
         in_edges_in_base = new_base.model_structure.sfd.in_edges(start_with_element_base)
@@ -246,7 +246,7 @@ class StructureUtilities(object):
         # pick an element from target_structure to start with. Now: randomly. Future: guided by activity.
         start_with_element_target = random.choice(target_structure_elements)
         print("    {} in target_structure is chosen to start with".format(start_with_element_target))
-        print("    Details: ", target_structure.model_structure.sfd.nodes[start_with_element_target])
+        # print("    Details: ", target_structure.model_structure.sfd.nodes[start_with_element_target])
 
         # get all in_edges into this element in target_structure
         in_edges_in_target = target_structure.model_structure.sfd.in_edges(start_with_element_target)
@@ -266,10 +266,10 @@ class StructureUtilities(object):
 
         # extract the part of structure containing this in_edge in target_structure
         subgraph_from_target = target_structure.model_structure.sfd.edge_subgraph([chosen_in_edge_in_target])
-        print("    Subgraph from target_structure:{} ".format(subgraph_from_target.nodes(data=True)))
+        print("    Subgraph from target_structure:{} ".format(subgraph_from_target.nodes(data='function')))
 
         new_base.model_structure.sfd = nx.compose(new_base.model_structure.sfd, subgraph_from_target)
-        print("New structure nodes:", new_base.model_structure.sfd.nodes.data())
+        print("New structure nodes:", new_base.model_structure.sfd.nodes.data('function'))
         print("New structure edges:", new_base.model_structure.sfd.edges.data())
 
         return new_base
@@ -307,7 +307,7 @@ class StructureManager(object):
         # 1. identical to base_structure it self
         # if nx.is_isomorphic(base_structure.model_structure.sfd, new_structure.model_structure.sfd):
         GM = iso.DiGraphMatcher(base_structure.model_structure.sfd, new_structure.model_structure.sfd,
-                                node_match=iso.categorical_node_match(attr=['equation'], default=[None])
+                                node_match=iso.categorical_node_match(attr=['function'], default=[None])
                                 # node_match=iso.categorical_node_match(attr=['equation', 'value'], default=[None, None])
                                 )
         if GM.is_isomorphic():
@@ -318,12 +318,14 @@ class StructureManager(object):
         for neighbour in self.tree.neighbors(self.get_uid_by_structure(base_structure)):
             GM = iso.DiGraphMatcher(self.tree.nodes[neighbour]['structure'].model_structure.sfd,
                                     new_structure.model_structure.sfd,
-                                    node_match=iso.categorical_node_match(attr=['equation'], default=[None])
+                                    node_match=iso.categorical_node_match(attr=['function'], default=[None])
                                     # node_match=iso.categorical_node_match(attr=['equation', 'value'], default=[None, None])
                                     )
             if GM.is_isomorphic():
             # if nx.is_isomorphic(self.tree.nodes[neighbour]['structure'].model_structure.sfd, new_structure.model_structure.sfd):
                 self.tree.nodes[neighbour]['activity'] += 1
+                # print(self.tree.nodes[neighbour]['structure'].model_structure.sfd.nodes(data=True))
+                # print(new_structure.model_structure.sfd.nodes(data=True))
                 print("    The new structure already exists in base structure's neighbours")
                 return
 
