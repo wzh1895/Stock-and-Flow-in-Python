60eabf297b4cb41d927753659ed0f9cf72eba2f7
wzh <rutherford1895@gmail.com>
2019-10-09 23:20:35 +0100

    Solved QGraphicsScene coordinates

--- StockAndFlowInPython/sfd_canvas/interactive_sfd.py
+++ StockAndFlowInPython/sfd_canvas/interactive_sfd.py
@@ -5,30 +5,26 @@ from PyQt5.QtWidgets import *
 from StockAndFlowInPython.sfd_canvas.interactive_sfd_ui import Ui_widget_interactive_sfd
 
 
-class InteractiveSFD(QGraphicsScene):
+class ModelCanvas(QGraphicsScene):
     def __init__(self):
-        super(InteractiveSFD, self).__init__()
-
-        self.n = 0
+        super(ModelCanvas, self).__init__()
+        self.working_mode = None
 
     def mousePressEvent(self, e):
-        print(e)
-        print('Press position:', e.pos().x(), e.pos().y())
-        self.add_stock(e.pos().x(), e.pos().y())
+        print(self.working_mode)
+        x = e.scenePos().x()
+        y = e.scenePos().y()
+        if self.working_mode == 'stock':
+            self.add_stock(x, y)
 
-    def mouseDoubleClickEvent(self, e):
-        print(e)
-        print('Double Click position:', e.pos().x(), e.pos().y())
-
-    def add_stock(self, x, y, w=50, h=30):
-        qrect = QRectF(self.n*20, self.n*20, w, h)
+    def add_stock(self, x, y, w=40, h=30):
+        qrect = QRectF(x-w*0.5, y-h*0.5, w, h)
         self.addItem(QGraphicsRectItem(qrect))
-        self.n+=1
 
 
-class WangSim(QWidget, Ui_widget_interactive_sfd):
+class InteractiveSFD(QWidget, Ui_widget_interactive_sfd):
     def __init__(self):
-        super(WangSim, self).__init__()
+        super(InteractiveSFD, self).__init__()
         self.setupUi(self)
         self.pushButton_add_stock.setCheckable(True)
         self.pushButton_add_flow.setCheckable(True)
@@ -40,33 +36,45 @@ class WangSim(QWidget, Ui_widget_interactive_sfd):
         self.pushButton_add_aux.clicked.connect(self.on_pushbutton_add_aux_pushed)
         self.pushButton_add_connector.clicked.connect(self.on_pushbutton_add_connector_pushed)
 
-        self.interactive_sfd = InteractiveSFD()
-        self.interactive_sfd.setSceneRect(0, 0, self.size().width(), self.size().height())
-        self.graphicsView_interactive_sfd.setScene(self.interactive_sfd)
+        self.model_canvas = ModelCanvas()
+        # self.sfd.setSceneRect(0, 0, self.width(), self.height())
+        # self.graphicsView_interactive_sfd.setFixedSize(self.width(), self.height())
+        # self.graphicsView_interactive_sfd.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.graphicsView_interactive_sfd.setSceneRect(0, 0, self.width(), self.height())
+        self.graphicsView_interactive_sfd.setScene(self.model_canvas)
 
     # Exclusively check buttons
     def on_pushbutton_add_stock_pushed(self):
+        self.model_canvas.working_mode = 'stock'
         self.pushButton_add_flow.setChecked(False)
         self.pushButton_add_aux.setChecked(False)
         self.pushButton_add_connector.setChecked(False)
+        if not self.pushButton_add_stock.isChecked():
+            self.model_canvas.working_mode = None
 
     def on_pushbutton_add_flow_pushed(self):
+        self.model_canvas.working_mode = 'flow'
         self.pushButton_add_stock.setChecked(False)
         self.pushButton_add_aux.setChecked(False)
         self.pushButton_add_connector.setChecked(False)
+        if not self.pushButton_add_flow.isChecked():
+            self.model_canvas.working_mode = None
 
     def on_pushbutton_add_aux_pushed(self):
+        self.model_canvas.working_mode = 'aux'
         self.pushButton_add_stock.setChecked(False)
         self.pushButton_add_flow.setChecked(False)
         self.pushButton_add_connector.setChecked(False)
+        if not self.pushButton_add_aux.isChecked():
+            self.model_canvas.working_mode = None
 
     def on_pushbutton_add_connector_pushed(self):
+        self.model_canvas.working_mode = 'connector'
         self.pushButton_add_stock.setChecked(False)
         self.pushButton_add_flow.setChecked(False)
         self.pushButton_add_aux.setChecked(False)
-
-    # def mousePressEvent(self, e):
-    #     print(e.x(), e.y())
+        if not self.pushButton_add_connector.isChecked():
+            self.model_canvas.working_mode = None
 
 
 if __name__ == '__main__':
@@ -74,7 +82,7 @@ if __name__ == '__main__':
     main_window = QMainWindow()
     main_window.setMinimumWidth(640)
     main_window.setMinimumHeight(480)
-    wang_sim = WangSim()
+    wang_sim = InteractiveSFD()
     main_window.setCentralWidget(wang_sim)
     main_window.show()
     sys.exit(app.exec_())
