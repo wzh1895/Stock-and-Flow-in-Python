ecb456934fde66f565d7dd4a10566167cc29e41e
rutherford1895 <rutherford1895@gmail.com>
2019-10-09 18:22:41 +0100

    Working on Interactive SFD Solved MouseEvent and how to use it in QGraphicsScene

--- StockAndFlowInPython/sfd_canvas/interactive_sfd.py
+++ StockAndFlowInPython/sfd_canvas/interactive_sfd.py
@@ -1,16 +1,73 @@
 import sys
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QApplication
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from StockAndFlowInPython.sfd_canvas.interactive_sfd_ui import Ui_widget_interactive_sfd
 
-class InteractiveSFD(QGraphicsView):
+
+class InteractiveSFD(QGraphicsScene):
     def __init__(self):
         super(InteractiveSFD, self).__init__()
-        self.scene = QGraphicsScene()
-        self.scene.addText('Hello')
-        self.setScene(self.scene)
+
+    def mousePressEvent(self, e):
+        print(e)
+        print('Press position:', e.pos().x(), e.pos().y())
+        self.add_stock()
+
+    def mouseDoubleClickEvent(self, e):
+        print(e)
+        print('Double Click position:', e.pos().x(), e.pos().y())
+
+    def add_stock(self):
+        qrect = QRectF(30, 30, 50, 30)
+        self.addItem(QGraphicsRectItem(qrect))
+
+
+class WangSim(QWidget, Ui_widget_interactive_sfd):
+    def __init__(self):
+        super(WangSim, self).__init__()
+        self.setupUi(self)
+        self.pushButton_add_stock.setCheckable(True)
+        self.pushButton_add_flow.setCheckable(True)
+        self.pushButton_add_aux.setCheckable(True)
+        self.pushButton_add_connector.setCheckable(True)
+
+        self.pushButton_add_stock.clicked.connect(self.on_pushbutton_add_stock_pushed)
+        self.pushButton_add_flow.clicked.connect(self.on_pushbutton_add_flow_pushed)
+        self.pushButton_add_aux.clicked.connect(self.on_pushbutton_add_aux_pushed)
+        self.pushButton_add_connector.clicked.connect(self.on_pushbutton_add_connector_pushed)
+
+        self.interactive_sfd = InteractiveSFD()
+        self.graphicsView_interactive_sfd.setScene(self.interactive_sfd)
+
+    # Exclusively check buttons
+    def on_pushbutton_add_stock_pushed(self):
+        self.pushButton_add_flow.setChecked(False)
+        self.pushButton_add_aux.setChecked(False)
+        self.pushButton_add_connector.setChecked(False)
+
+    def on_pushbutton_add_flow_pushed(self):
+        self.pushButton_add_stock.setChecked(False)
+        self.pushButton_add_aux.setChecked(False)
+        self.pushButton_add_connector.setChecked(False)
+
+    def on_pushbutton_add_aux_pushed(self):
+        self.pushButton_add_stock.setChecked(False)
+        self.pushButton_add_flow.setChecked(False)
+        self.pushButton_add_connector.setChecked(False)
+
+    def on_pushbutton_add_connector_pushed(self):
+        self.pushButton_add_stock.setChecked(False)
+        self.pushButton_add_flow.setChecked(False)
+        self.pushButton_add_aux.setChecked(False)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    interactive_sfd = InteractiveSFD()
-    interactive_sfd.show()
+    main_window = QMainWindow()
+    main_window.setMinimumWidth(640)
+    main_window.setMinimumHeight(480)
+    wang_sim = WangSim()
+    main_window.setCentralWidget(wang_sim)
+    main_window.show()
     sys.exit(app.exec_())
---
+++ StockAndFlowInPython/sfd_canvas/interactive_sfd_ui.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'interactive_sfd_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_widget_interactive_sfd(object):
+    def setupUi(self, widget_interactive_sfd):
+        widget_interactive_sfd.setObjectName("widget_interactive_sfd")
+        widget_interactive_sfd.resize(640, 480)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(widget_interactive_sfd.sizePolicy().hasHeightForWidth())
+        widget_interactive_sfd.setSizePolicy(sizePolicy)
+        self.verticalLayout = QtWidgets.QVBoxLayout(widget_interactive_sfd)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame_modifier = QtWidgets.QFrame(widget_interactive_sfd)
+        self.frame_modifier.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_modifier.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_modifier.setObjectName("frame_modifier")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.frame_modifier)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_add_stock = QtWidgets.QPushButton(self.frame_modifier)
+        self.pushButton_add_stock.setObjectName("pushButton_add_stock")
+        self.horizontalLayout.addWidget(self.pushButton_add_stock)
+        self.pushButton_add_flow = QtWidgets.QPushButton(self.frame_modifier)
+        self.pushButton_add_flow.setObjectName("pushButton_add_flow")
+        self.horizontalLayout.addWidget(self.pushButton_add_flow)
+        self.pushButton_add_aux = QtWidgets.QPushButton(self.frame_modifier)
+        self.pushButton_add_aux.setObjectName("pushButton_add_aux")
+        self.horizontalLayout.addWidget(self.pushButton_add_aux)
+        self.pushButton_add_connector = QtWidgets.QPushButton(self.frame_modifier)
+        self.pushButton_add_connector.setObjectName("pushButton_add_connector")
+        self.horizontalLayout.addWidget(self.pushButton_add_connector)
+        self.verticalLayout.addWidget(self.frame_modifier)
+        self.graphicsView_interactive_sfd = QtWidgets.QGraphicsView(widget_interactive_sfd)
+        self.graphicsView_interactive_sfd.setObjectName("graphicsView_interactive_sfd")
+        self.verticalLayout.addWidget(self.graphicsView_interactive_sfd)
+
+        self.retranslateUi(widget_interactive_sfd)
+        QtCore.QMetaObject.connectSlotsByName(widget_interactive_sfd)
+
+    def retranslateUi(self, widget_interactive_sfd):
+        _translate = QtCore.QCoreApplication.translate
+        widget_interactive_sfd.setWindowTitle(_translate("widget_interactive_sfd", "Form"))
+        self.pushButton_add_stock.setText(_translate("widget_interactive_sfd", "Stock"))
+        self.pushButton_add_flow.setText(_translate("widget_interactive_sfd", "Flow"))
+        self.pushButton_add_aux.setText(_translate("widget_interactive_sfd", "Aux"))
+        self.pushButton_add_connector.setText(_translate("widget_interactive_sfd", "Connector"))
